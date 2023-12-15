55240fb407ab427b893bc2db10cc0c53d34b4629
Zakaria <46360544+zakaria29@users.noreply.github.com>
2023-12-15 02:12:15 +0000

    fixing not found

--- src/pages/Notfound.js
+++ src/pages/Notfound.js
@@ -8,7 +8,7 @@ export default function NotFound() {
                     <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-red-600">ERROR🤪</h1>
                     <p className="mb-4 text-3xl tracking-tight font-bold md:text-4xl text-black">Something's missing.</p>
                     <p className="mb-4 text-lg font-light text-gray-400">Sorry, we can't find that page. </p>
-                    <a href="/" className="inline-flex text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center focus:ring-red-900 my-4">Back to Login</a>
+                    <a href="/magangjesicatamufe/build/" className="inline-flex text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center focus:ring-red-900 my-4">Back to Login</a>
                 </div>
             </div>
         </section>
